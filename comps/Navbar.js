8ff1af972a67dcda6f7700411226176597cb24d1
Richard <richard.yang.tong@gmail.com>
2021-04-11 19:50:31 -0400

    Using next image feature to lazy load the image.

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,10 +1,15 @@
-import Link from 'next/link';
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Navbar = () => {
     return ( 
         <nav>
             <div className="logo">
-                <img src="/logo.png"/>
+                <Image 
+                    src="/logo.png"
+                    width={128}
+                    height={77}
+                />
             </div>
             <Link href="/">Home</Link>
             <Link href="/about">About</Link>
