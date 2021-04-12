893a40db9e6d5c5439baa8b086923819b68833e9
Richard <richard.yang.tong@gmail.com>
2021-04-12 14:00:00 -0400

    Fixed sever bugs that causing api fetch fails.

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,38 +1,39 @@
 export const getStaticPaths = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await res.json();
-
+  
+    // map data to an array of path objects with params (id)
     const paths = data.map(ninja => {
-        return {
-            params: { id: ninja.id.toString() }
-        }
+      return {
+        params: { id: ninja.id.toString() }
+      }
     })
-
+  
     return {
-        paths,
-        fallback: false
+      paths,
+      fallback: false
     }
-}
-
-export const getStaticProps = async (context) => {
+  }
+  
+  export const getStaticProps = async (context) => {
     const id = context.params.id;
-    const res = await fetch('https://jsonplaceholder.typicode.com/users' + id);
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
     const data = await res.json();
-    
+  
     return {
-        props: { ninja: data }
+      props: { ninja: data }
     }
-}
-
-const Details = ({ ninja }) => {
+  }
+  
+  const Details = ({ ninja }) => {
     return (
-        <div>
-            <h1>{ ninja.name }</h1>
-            <p>{ ninja.email }</p>
-            <p>{ ninja. website }</p>
-            <p>{ ninja.address.city }</p>
-        </div>
+      <div>
+        <h1>{ ninja.name }</h1>
+        <p>{ ninja.email }</p>
+        <p>{ ninja.website }</p>
+        <p>{ ninja.address.city }</p>
+      </div>
     );
-}
- 
-export default Details;
+  }
+  
+  export default Details;
