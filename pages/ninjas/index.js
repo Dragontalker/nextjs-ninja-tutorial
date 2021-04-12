5c33e3ee1603667122c24f6089d32ad6db03ba4a
Richard <richard.yang.tong@gmail.com>
2021-04-12 06:55:13 -0400

    Applied style to ninja lists.

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,3 +1,5 @@
+import styles from '../../styles/Ninjas.module.css'
+
 export const getStaticProps = async () => {
 
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
@@ -14,7 +16,7 @@ const Ninjas = ({ ninjas }) => {
             <h1>All Ninjas</h1>
             {ninjas.map(ninja => (
                 <div key={ninja.id}>
-                    <a>
+                    <a className="single">
                         <h3>{ninja.name}</h3>
                     </a>
                 </div>
